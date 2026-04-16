67bf9ce8144d604efb8b6769f174871fe0e6c516
gai-one1 <msrtinez.ronan@hotmail.com>
2026-04-15 20:06:10 -0500

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const incomingUrl = new URL(context.request.url)
 
-  const target = new URL("https://sage-saw-distributors-brooks.trycloudflare.com")
+  const target = new URL("https://implications-arctic-enough-reflects.trycloudflare.com")
   target.pathname = incomingUrl.pathname || ""
   target.search = incomingUrl.search
 
